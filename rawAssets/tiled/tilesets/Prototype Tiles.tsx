d08f618c0c8b50448a369908e7599fd07589b007
Drew Gingerich <gingerich93@gmail.com>
2018-12-01 14:17:52 -0800

    Add json files for tiled.

--- rawAssets/tiled/tilesets/Prototype Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="Prototype Tiles" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../../aseprite/prototype_tiles.png" width="64" height="64"/>
-</tileset>
